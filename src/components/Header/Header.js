ff156f9d0206eda3fef88f60646a36e641a989fb
tomaszporeba <tomaszporeba1@gmail.com>
2019-02-28 22:00:12 +0100

    header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,7 @@ export default ({toggleSidebar}) => {
     return (
         <div className="header-container">
             <i onClick={toggleSidebar} className="small material-icons burger-icon">menu</i>
-
-            {/*<span>{`${user.firstName} ${user.lastName}`}</span>*/}
+            <span>{`${user.firstName} ${user.lastName}`}</span>
         </div>
     );
 }
